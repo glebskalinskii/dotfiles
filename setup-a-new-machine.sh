1f1efa942507b6cffa3cff60092007cd8d3c38b2
Gleb Skalinskii <svgolova@gmail.com>
2015-03-13 20:56:54 +0000

    oh-my-zsh

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
