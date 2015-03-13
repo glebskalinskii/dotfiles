51ff1d8ef6b77dc53380e5d3efa965d1764c2cb0
Gleb Skalinskii <svgolova@gmail.com>
2015-03-13 21:00:36 +0000

    karabiner skype

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,11 +6,13 @@ brew tap caskroom/versions
 # daily
 brew cask install alfred
 brew cask install dropbox
+brew cask install skype
 
 # dev
 brew cask install sublime-text
 brew cask install imagealpha
 brew cask install imageoptim
+brew cask install karabiner
 
 # browsers
 brew cask install google-chrome-canary
